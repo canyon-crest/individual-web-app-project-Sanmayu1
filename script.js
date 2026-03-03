1bd3bc3a9e542e0ddda950631eb4692f7799dab2
Sanmayu1 <nandipatis4941@my.sduhsd.net>
2026-03-03 21:51:33 +0000

    day 2, html css

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// add javascript here
+document.getElementById("myBtn")?.addEventListener("click", () => {
+    document.getElementById("my-offeringa").toggleAttribute("hidden");
+})
+
+document.getElementById("taskBtn")?.addEventListener("click", () =>{
+    let task = document.getElementById("task");
+    let tasklist = document.getElementById("taskList");
+    if(task.ariaValueMax.trim()!= ""){
+        const newTask = document.createElement("li");
+        newTask.textContent = task.values;
+        tasklist.appendChild(newTask);
+        task.value = "";
+    }
+})
